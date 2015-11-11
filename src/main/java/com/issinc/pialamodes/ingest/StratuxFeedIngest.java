95834dd55bd44d3b09f23fc175169353cf45251a
AlthF <danielskeating@gmail.com>
2015-11-11 12:29:51 -0700

    Update StratuxFeedIngest.java

--- src/main/java/com/issinc/pialamodes/ingest/StratuxFeedIngest.java
+++ src/main/java/com/issinc/pialamodes/ingest/StratuxFeedIngest.java
@@ -1,10 +1,14 @@
 package com.issinc.pialamodes.ingest;
 
+import static com.jayway.restassured.RestAssured.given;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,59 +20,122 @@ public class StratuxFeedIngest
     IPositionService service;
     String host = "172.20.6.146";
     int port = 30003;
-    
+
     public void readFeed(String host, int port)
     {
         this.host = host;
         this.port = port;
         readFeed();
     }
-    
+
     public void readFeed()
     {
         try (Socket socket = new Socket(host, port);
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));)
-        {
-                String line;
-                
-                while ((line = in.readLine()) != null)
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));)
                 {
-                    String[] lineParts = line.split(",");
-                    if (lineParts[0].equals("MSG"))
+            String line;
+
+            while ((line = in.readLine()) != null)
+            {
+                log.info(line);
+                String[] lineParts = line.split(",");
+                if (lineParts[0].equals("MSG"))
+                {
+                    String id = lineParts[4];
+                    //ID Message
+                    if (lineParts[1].equals("1"))
                     {
-                        String id = lineParts[4];
-                        //ID Message
-                        if (lineParts[1].equals("1"))
+                        try
                         {
-                            String callSign = lineParts[10];
-                            log.warn(id + " has callsign " + callSign);
+                            JSONObject callsign = new JSONObject();
+                            callsign.put("hexIdent", id);
+                            callsign.put("callsign", lineParts[10]);
+                            String jsonString = callsign.toString();
+
+                            given().
+                            contentType("application/json").
+                            body(jsonString).
+                            when().
+                            post("/callsign");
                         }
-                        //Surface Position Message
-                        else if (lineParts[1].equals("2"))
+                        catch (JSONException e)
                         {
-                            String altitude = lineParts[11];
-                            String groundSpeed = lineParts[12];
-                            String latitude = lineParts[14];
-                            String longitude = lineParts[15];
-                            
+                            log.error("Failed to convert to JSON : " + e.getMessage());
                         }
-                        //Airborne Position Message
-                        else if (lineParts[1].equals("3"))
+                    }
+                    //Surface Position Message
+                    else if (lineParts[1].equals("2"))
+                    {
+                        try
                         {
-                            String altitude = lineParts[11];
-                            String latitude = lineParts[14];
-                            String longitude = lineParts[15];
-                            
+                            JSONObject position = new JSONObject();
+                            position.put("hexIdent", id);
+                            position.put("lat", Double.valueOf(lineParts[14]));
+                            position.put("lon", Double.valueOf(lineParts[15]));
+                            position.put("heading", Double.valueOf(lineParts[13]));
+                            position.put("groundSpeed", Double.valueOf(lineParts[12]));
+                            String jsonString = position.toString();
+
+                            given().
+                            contentType("application/json").
+                            body(jsonString).
+                            when().
+                            post("/position");
                         }
-                        //Airborne Velocity Message
-                        else if (lineParts[1].equals("4"))
+                        catch (JSONException e)
                         {
-                            String groundSpeed = lineParts[12];
-                            String verticalRate = lineParts[16];
+                            log.error("Failed to convert to JSON : " + e.getMessage());
+                        }
+
+                    }
+                    //Airborne Position Message
+                    else if (lineParts[1].equals("3"))
+                    {
+                        try
+                        {
+                            JSONObject position = new JSONObject();
+                            position.put("hexIdent", id);
+                            position.put("lat", Double.valueOf(lineParts[14]));
+                            position.put("lon", Double.valueOf(lineParts[15]));
+                            String jsonString = position.toString();
+
+                            given().
+                            contentType("application/json").
+                            body(jsonString).
+                            when().
+                            post("/position");
+                        }
+                        catch (JSONException e)
+                        {
+                            log.error("Failed to convert to JSON : " + e.getMessage());
+                        }
+                    }
+                    //Airborne Velocity Message
+                    else if (lineParts[1].equals("4"))
+                    {
+                        try
+                        {
+                            JSONObject position = new JSONObject();
+                            position.put("hexIdent", id);
+                            position.put("heading", Double.valueOf(lineParts[13]));
+                            position.put("groundSpeed", Double.valueOf(lineParts[12]));
+                            position.put("verticalRate", Double.valueOf(lineParts[16]));
+                            String jsonString = position.toString();
+
+                            given().
+                            contentType("application/json").
+                            body(jsonString).
+                            when().
+                            post("/position");
+                        }
+                        catch (JSONException e)
+                        {
+                            log.error("Failed to convert to JSON : " + e.getMessage());
                         }
                     }
                 }
-        }
+            }
+                }
         catch (IOException e)
         {
             log.error(e.getStackTrace().toString());
