363ee7c5db0c7d99cfd0ccda2bc0555f510bd454
Jay Moss <jay.moss@issinc.com>
2015-11-11 15:19:38 -0700

    update pom to set rest-assured libs as test scope

--- src/main/java/com/issinc/pialamodes/ServerApplication.java
+++ src/main/java/com/issinc/pialamodes/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.issinc.pialamodes;
 
+import com.issinc.pialamodes.ingest.StratuxFeedIngest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
